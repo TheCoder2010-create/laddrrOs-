339dd091adfabb473bdee011ce31f20e0a2070f5
AccountabilityOS <strepxe.os@gmail.com>
2025-10-14 07:58:51 +0000

    on team lead profile 1-on-1 Quality Score Trend the chart's data is outs

--- src/components/dashboards/quality-score-trend-widget.tsx
+++ src/components/dashboards/quality-score-trend-widget.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useState, useEffect, useCallback } from 'react';
@@ -63,7 +64,7 @@ export default function QualityScoreTrendWidget() {
             >
               <LineChart
                 data={chartData}
-                margin={{ top: 5, right: 20, left: -10, bottom: 0 }}
+                margin={{ top: 5, right: 20, left: 0, bottom: 0 }}
               >
                 <CartesianGrid vertical={false} />
                 <XAxis dataKey="date" tickLine={false} axisLine={false} tickMargin={8} />
