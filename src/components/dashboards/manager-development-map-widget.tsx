0cbbe7b62da3442bfaa90e13c9569ffe353a2dc1
AccountabilityOS <strepxe.os@gmail.com>
2025-10-16 08:36:20 +0000

    Reduce the header size to match the text size (_for element <CardHeader>

--- src/components/dashboards/manager-development-map-widget.tsx
+++ src/components/dashboards/manager-development-map-widget.tsx
@@ -40,8 +40,8 @@ export default function ManagerDevelopmentMapWidget() {
 
   return (
     <Card>
-      <CardHeader>
-        <CardTitle className="flex items-center gap-2">
+      <CardHeader className="p-2">
+        <CardTitle className="flex items-center gap-2 text-base">
           <Map className="text-primary" />
           Manager Development Map
         </CardTitle>
